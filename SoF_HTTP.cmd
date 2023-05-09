799a0e6b95c3cb646c8f5d35007093619929d63e
Graham Fenner <d3nd3second@gmail.com>
2023-05-09 02:28:53 +0100

    File path quoted for space and support cmd line options

--- SoF_HTTP.cmd
+++ SoF_HTTP.cmd
@@ -1,1 +1,3 @@
-powershell.exe -ExecutionPolicy Bypass -File http_listener.ps1
+REM Define the SOF_OPTIONS variable
+SET "SOF_OPTIONS=+set console 1 +set nope lol"
+powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File http_listener.ps1 %SOF_OPTIONS%
--- http_listener.ps1
+++ http_listener.ps1
@@ -1,5 +1,6 @@
 if ( $args -contains "disappear" ) {
 
+$arguments = $args[1..($args.Length - 1)]
 $ErrorActionPreference = 'Stop'
 
 $HttpFile = ''
@@ -8,68 +9,69 @@ $HttpServer = 'https://raw.githubusercontent.com/plowsof/sof1maps/main/'
 
 # Delete existing http file if it exists
 If (Test-Path $AFile) {
-	Remove-Item $AFile
-	Write-Host 'Deleting existing http file.'
+    Remove-Item $AFile
+    Write-Host 'Deleting existing http file.'
 }
 
 Write-Host 'Welcome to SoF1 HTTP Auto Map Downloader.'
 Write-Host 'Keep this window open so that it can detect map change.'
 Write-Host 'SoF should open automatically now.'
 
-# Launch SoF
-$gameProcess = Start-Process -PassThru -FilePath 'SoF.exe' -ArgumentList "+set console 1"
+# Launch SoF - PassThru required to kill later.
+$gameProcess = Start-Process -PassThru -FilePath 'SoF.exe' -ArgumentList $arguments
 
 While ($true) {
-	If (Test-Path $AFile) {
-		# Read the last line of the mapname file
-		$HttpFile = Get-Content $AFile | Select-Object -Last 1
-		
-		$HttpFile = $HttpFile.Split('"')[3]
-		Write-Host "Map name: $HttpFile"
-		
-		# Remove .bsp extension and add .zip extension
-		$HttpFile = $HttpFile -replace '\.bsp$', '.zip'
+    If (Test-Path $AFile) {
+        # Read the last line of the mapname file
+        $HttpFile = Get-Content $AFile | Select-Object -Last 1
+        
+        $HttpFile = $HttpFile.Split('"')[3]
+        Write-Host "Map name: $HttpFile"
+        
+        # Remove .bsp extension and add .zip extension
+        $HttpFile = $HttpFile -replace '\.bsp$', '.zip'
 
-		# Download the file from the http server
-		$DestFile = Join-Path 'user\maps' $HttpFile
+        # Download the file from the http server
+        $DestFile = Join-Path 'user\maps' $HttpFile
 
-		# Create the directory if it doesn't exist
-		$directory = Split-Path -Path $DestFile -Parent
-		if (!(Test-Path -Path $directory -PathType Container)) {
-			New-Item -Path $directory -ItemType Directory | Out-Null
-		}
-		# Download the file using Invoke-WebRequest
-		Write-Host "NEUTRAL: Trying to download ""$HttpServer$HttpFile"" to ""$DestFile""."
+        # Create the directory if it doesn't exist
+        $directory = Split-Path -Path $DestFile -Parent
+        if (!(Test-Path -Path $directory -PathType Container)) {
+            New-Item -Path $directory -ItemType Directory | Out-Null
+        }
+        # Download the file using Invoke-WebRequest
+        Write-Host "NEUTRAL: Trying to download ""$HttpServer$HttpFile"" to ""$DestFile""."
 
-		try {
-			$ProgressPreference = 'SilentlyContinue'
-			Invoke-WebRequest -Uri "$HttpServer$HttpFile" -OutFile $DestFile -ErrorAction Stop
-			Write-Host "SUCCESS: ""$HttpFile"" downloaded to ""$DestFile""."
+        try {
+            $ProgressPreference = 'SilentlyContinue'
+            Invoke-WebRequest -Uri "$HttpServer$HttpFile" -OutFile $DestFile -ErrorAction Stop
+            Write-Host "SUCCESS: ""$HttpFile"" downloaded to ""$DestFile""."
 
-			# Read-Host -Prompt "Press Enter to continue..."
+            # Read-Host -Prompt "Press Enter to continue..."
 
-			# Extract the zip file
-			Expand-Archive -LiteralPath $DestFile -DestinationPath 'user' -Force
+            # Extract the zip file
+            Expand-Archive -LiteralPath $DestFile -DestinationPath 'user' -Force
 
-			# Delete the zip file
-			Remove-Item $DestFile
-			
-		} catch {
-			# 404 error catch
-			Write-Host "ERROR: $($_.Exception.Message)"
-		}
-		# Delete the http file
-		Remove-Item $AFile
-	}
-	# Wait for 2 seconds and check again
-	Start-Sleep -Seconds 2
+            # Delete the zip file
+            Remove-Item $DestFile
+            
+        } catch {
+            # 404 error catch
+            Write-Host "ERROR: $($_.Exception.Message)"
+        }
+        # Delete the http file
+        Remove-Item $AFile
+    }
+    # Wait for 2 seconds and check again
+    Start-Sleep -Seconds 2
 
-	if ($gameProcess.HasExited) {
-		exit
-	}
+    if ($gameProcess.HasExited) {
+        exit
+    }
 
 }
 } else {
-	$arguments = "-WindowStyle Hidden", "-File", $PSCommandPath, "disappear"
-	Start-Process "powershell.exe" -ArgumentList $arguments
+    # $arguments = "-ExecutionPolicy Bypass", "-File `"$PSCommandPath`"", "disappear", "$args"
+    $arguments = "-WindowStyle Hidden","-ExecutionPolicy Bypass", "-File `"$PSCommandPath`"", "disappear", "$args"
+    Start-Process "powershell.exe" -ArgumentList $arguments
 }

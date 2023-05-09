003e0c094d46fa688f7988b021fc4f07a4a82fdc
Graham Fenner <d3nd3second@gmail.com>
2023-05-09 02:37:57 +0100

    remove junk

--- SoF_HTTP.cmd
+++ SoF_HTTP.cmd
@@ -1,3 +1,3 @@
 REM Define the SOF_OPTIONS variable
-SET "SOF_OPTIONS=+set console 1 +set nope lol"
+SET "SOF_OPTIONS=+set console 1"
 powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File http_listener.ps1 %SOF_OPTIONS%
